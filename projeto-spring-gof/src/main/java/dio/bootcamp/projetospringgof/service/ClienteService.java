7cc4439e8f62e52c49ea1c1895c91ac21633527e
Manu Brederode <brederode.emmanuelle@gmail.com>
2024-07-28 20:08:36 +0100

    Add files via upload

---
+++ projeto-spring-gof/src/main/java/dio/bootcamp/projetospringgof/service/ClienteService.java
@@ -0,0 +1,25 @@
+package dio.bootcamp.projetospringgof.service;
+
+import dio.bootcamp.projetospringgof.model.Cliente;
+import org.springframework.stereotype.Service;
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ *
+ */
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
---
+++ projeto-spring-gof/src/test/java/dio/bootcamp/projetospringgof/ProjetoSpringGofApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.bootcamp.projetospringgof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringGofApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
